e0a42f866ff1d2775760d5d2d04486a214ee3a2a
rarwrly <ehc2642@gmail.com>
2020-02-26 23:36:38 -0500

    Calculator with number pad, clear button and decimal support

--- homework01.js
+++ homework01.js
@@ -10,44 +10,65 @@ var division = document.getElementById('button-division')
 var equals = document.getElementById('button-equals')
 var operation
 var result =document.getElementById('result')
- 
+var clear =document.getElementById('button-clear')
+var decimal = document.getElementById('button-decimal')
+ /* get numpad */
+
+ for (var i=0; i <= 9; i++){
+ 	var  numbutton =document.getElementById('button-'+i)
+ 		numbutton.addEventListener('click', function(e){
+ 		numText.value= numText.value + e.target.innerHTML
+ 	})
+ }
+
+decimal.addEventListener('click', function(){
+numText.value= numText.value + '.'
+
+ 	})
+
 addition.addEventListener('click', function (){ 
  	operation = '+'
-    val1 = parseInt(numText.value)
+    val1 = parseFloat(numText.value)
     numText.value=''
  })
 
   subtraction.addEventListener('click', function (){ 
  	operation = '-'
-     val1 = parseInt(numText.value)
+     val1 = parseFloat(numText.value)
     numText.value=''    
  })
     multiplication.addEventListener('click', function (){ 
  	operation = '*'
-    val1 = parseInt(numText.value)
+    val1 = parseFloat(numText.value)
     numText.value=''     
  })
-     division.addEventListener('click', function (){ 
+    division.addEventListener('click', function (){ 
  	operation = '/'
-    val1 = parseInt(numText.value)
+    val1 = parseFloat(numText.value)
     numText.value=''     
  })
 equals.addEventListener('click', function(){
-	val2 = parseInt(numText.value)
+	val2 = parseFloat(numText.value)
 	if ( operation == '+'){
-	 		result.innerHTML = val1 + val2 
+	 		numText.value = val1 + val2 
 	 	}
 	 	else if (operation == '-'){
-	 		result.innerHTML = val1 - val2
+	 		numText.value = val1 - val2
 	 	}
 	 	else if (operation == '*'){
-	 		result.innerHTML = val1 * val2
+	 		numText.value = val1 * val2
 	 	}
 	 	else if (operation == '/') {
-	 	result.innerHTML = val1 / val2
+	 		numText.value = val1 / val2
 	 }
  operation = undefined
-	numText.value =''
  /*val1.value =''
  val2.value =''*/
 })
+    clear.addEventListener('click', function (){ 
+    numText.value='' 
+    val1 = undefined
+    val2 = undefined
+    operation =undefined
+ })
+
